fd51e9c5ef403f806c2503e494d361285e39baf8
Muhamad Rafli Andrea Bastedo <muhamad.rafli@compnet.co.id>
2026-08-10 14:01:36 +0700

    drop route:cache (closure route in api.php)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,6 @@ chown -R www-data:www-data storage bootstrap/cache
 
 php artisan migrate --force --no-interaction
 php artisan config:cache
-php artisan route:cache
 php artisan view:cache
 
 exec frankenphp run --config /app/Caddyfile
