46bcc3b321ae077ab76612444ddb2ac7998f0422
Muhamad Rafli Andrea Bastedo <muhamad.rafli@compnet.co.id>
2026-08-12 08:23:05 +0700

    Redeploy

--- config/database.php
+++ config/database.php
@@ -100,6 +100,22 @@ return [
             'endpoint_id' => env('DB_ENDPOINT_ID'),
         ],
 
+        'pgsql_direct' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_URL_DIRECT'),
+            'host' => env('DB_HOST_DIRECT', str_replace('-pooler', '', env('DB_HOST', '127.0.0.1'))),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'endpoint_id' => env('DB_ENDPOINT_ID'),
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
